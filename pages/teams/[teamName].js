b7a9b43858160d60ee09db554cbf6f4103086cfc
Maria Nery <67118326+mnvnery@users.noreply.github.com>
2022-12-20 13:15:14 +0000

    useEffect

--- pages/teams/[teamName].js
+++ pages/teams/[teamName].js
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { QRCodeSVG } from 'qrcode.react'
 import socket from '../../utils/sockets'
 
